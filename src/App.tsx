0fa4639a2dd5446a5dfa30a4439a7b6c44c3965e
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2023-06-22 23:15:46 +0930

    added edges

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import ReactFlow, { Background, Controls, Node } from 'reactflow';
+import ReactFlow, { Background, Connection, Controls, Node, addEdge, useEdgesState } from 'reactflow';
 import 'reactflow/dist/style.css';
 
 import {zinc} from 'tailwindcss/colors';
 import { Square } from './components/nodes/Square';
+import { useCallback } from 'react';
 
 const initialNodes = [
   { 
@@ -24,13 +25,20 @@ const NODE_TYPES = {
 };
 
 function App() {
-
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  console.log(edges)
+  const onConnect = useCallback((connection: Connection)=>{
+    return setEdges((edges)=> addEdge(connection, edges))
+  },[])
 
   return (
     <div className='w-screen h-screen'>
       <ReactFlow
         nodeTypes={NODE_TYPES}
         nodes={initialNodes}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
       >
         <Background 
           gap={12}
